cb49fbdacb87bd6b8fa36644036afa663259bb1c
crbby <koscikpatryk@gmail.com>
2022-06-25 23:26:01 +0200

    [scripts] Disabled buffering in print()

--- make.sh
+++ make.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+export PYTHONUNBUFFERED=TRUE
+
 echo "[runner] Running script in a verbose mode"
 echo "[runner] Compiling TeX files using compile_all_docker.py"
-python3 compile_all_docker.py
+python3 -u compile_all_docker.py
 
 echo "[runner] Merging all files using merge_all.py"
-python3 merge_all.py
+python3 -u merge_all.py
 
 echo "[runner] Compressing all files using compress.sh"
 ./compress.sh
