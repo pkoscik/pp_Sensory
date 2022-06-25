8bd2d98ca25d70ed33339d65ea7ac9d129091e76
crbby <koscikpatryk@gmail.com>
2022-06-25 22:48:36 +0200

    [scripts] Fixed make.sh

--- make.sh
+++ make.sh
@@ -1,58 +1,14 @@
 #!/bin/bash
 
-while getopts "vh" opt; do
-  case $opt in
-    v)
-      echo "Running script in a verbose mode"
+echo "[runner] Running script in a verbose mode"
+echo "[runner] Compiling TeX files using compile_all_docker.py"
+python3 compile_all_docker.py
 
-      echo "Compiling TeX files using compile_all_docker.py"
-      python3 compile_all_docker.py
-      
-      echo "Merging all files using merge_all.py"
-      python3 merge_all.py
-      
-      echo "Compressing all files using compress.sh"
-      ./compress.sh
-      
-      while true; do
-      read -p "Do you wish to remove uncompressed pdf file? " yn
-      case $yn in
-          [Yy]* ) rm out.pdf; break;;
-          [Nn]* ) exit;;
-          * ) echo "Please answer yes or no.";;
-      esac
-      done
-      exit 0
-      ;;
+echo "[runner] Merging all files using merge_all.py"
+python3 merge_all.py
 
-    h)
-      echo "usage: make.sh -(v/h)"
-      echo "    -v -> verbose tag"
-      echo "    -h -> help dialog"
-      exit 0
-      ;;
-  esac
-done
+echo "[runner] Compressing all files using compress.sh"
+./compress.sh
 
-echo "Running script in a non-verbose mode"
-
-echo "Compiling TeX files using compile_all_docker.py"
-python3 compile_all_docker.py >/dev/null
-
-echo "Merging all files using merge_all.py"
-python3 merge_all.py >/dev/null
-
-echo "Compressing all files using compress.sh"
-./compress.sh >/dev/null
-
-while true; do
-    read -p "Do you wish to remove uncompressed pdf file? " yn
-    case $yn in
-        [Yy]* ) rm out.pdf; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-exit 0
-
-
+echo "[runner] Removing uncompressed file"
+rm out.pdf
